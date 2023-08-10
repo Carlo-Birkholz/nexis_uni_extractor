28b9233344cec271b1ed871581a12d715c748ba4
zhong3401 <104781290+zhong3401@users.noreply.github.com>
2023-08-10 10:26:43 +0200

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,6 +1,6 @@
 from pipeline import convert_files_to_csv
 
-input_folder = 'E:\\github\\nexis_uni_extractor\\test\\2013'
-output_folder = 'E:\\github\\nexis_uni_extractor\\test\\2013'
+input_folder = 'test\\2013'
+output_folder = 'test\\2013'
 
 convert_files_to_csv(input_folder, output_folder, language='french')
